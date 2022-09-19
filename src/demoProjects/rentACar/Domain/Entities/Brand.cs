1106fdab64e292e283e6a6d9d048e79d843917a1
Özer Kutlu ÜNAL <ozer@unal-group.com>
2022-09-19 23:33:36 +0300

    4. Gün Dersi

--- src/demoProjects/rentACar/Domain/Entities/Brand.cs
+++ src/demoProjects/rentACar/Domain/Entities/Brand.cs
@@ -10,10 +10,10 @@ namespace Domain.Entities
     public class Brand : Entity
     {
         public string Name { get; set; }
+        public virtual ICollection<Model> Models { get; set; }
 
         public Brand()
         {
-
         }
 
         public Brand(int id, string name) : this()
